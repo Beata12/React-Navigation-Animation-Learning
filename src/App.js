38058ed060c71b21bf46e9c9a8dca55ac4396698
Beata12 <beata.marek.94@gmail.com>
2023-05-23 19:25:32 +0200

    cm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navigation from "./pages/Navigation";
 import Study from "./pages/Study";
--- src/pages/Decisions.js
+++ src/pages/Decisions.js
@@ -2,20 +2,23 @@ import React from "react";
 
 const Decisions = () => {
 	return (
-		<div className="text-box">
-			The beauty of life lies in its diversity. Each person's journey is
-			unique, shaped by their choices, circumstances, and the people they
-			encounter along the way. Life presents us with countless
-			opportunities to learn, explore, and discover our passions and
-			purpose. It encourages us to step out of our comfort zones, face our
-			fears and embrace new experiences. Just like an adventure, life is
-			not without its obstacles. We encounter difficulties and setbacks,
-			but it is through these challenges that we learn and grow. They
-			shape our character, teach us resilience, and help us appreciate the
-			moments of triumph even more. Moreover. Relationships, friendships,
-			and love are an integral part of this journey. The shared
-			experiences, support, and love we receive enrich our lives and make
-			the adventure all the more meaningful.
+		<div>
+			<div className="text-box ">
+				The beauty of life lies in its diversity. Each person's journey
+				is unique, shaped by their choices, circumstances, and the
+				people they encounter along the way. Life presents us with
+				countless opportunities to learn, explore, and discover our
+				passions and purpose. It encourages us to step out of our
+				comfort zones, face our fears and embrace new experiences. Just
+				like an adventure, life is not without its obstacles. We
+				encounter difficulties and setbacks, but it is through these
+				challenges that we learn and grow. They shape our character,
+				teach us resilience, and help us appreciate the moments of
+				triumph even more. Moreover. Relationships, friendships, and
+				love are an integral part of this journey. The shared
+				experiences, support, and love we receive enrich our lives and
+				make the adventure all the more meaningful.
+			</div>
 		</div>
 	);
 };
